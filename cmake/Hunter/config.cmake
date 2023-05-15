0e1fbcf4bbab70fda56b7b2ddfa99c3ac981374c
Andrey Galkin <andrey@futoin.org>
2023-05-15 13:40:07 +0300

    CHANGED: dependency maintenance

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,4 @@
+
+include(${CMAKE_CURRENT_LIST_DIR}/../../externals/core-cpp-api/cmake/Hunter/config.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../../externals/core-cpp-ri-asyncsteps/cmake/Hunter/config.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../../externals/core-cpp-ri-asyncevent/cmake/Hunter/config.cmake)
